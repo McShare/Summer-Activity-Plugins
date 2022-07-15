e82ef54cfab4e8cb03f4d14e1aaba46c9f32a22f
McPlus <97387028+McPlusss@users.noreply.github.com>
2022-07-15 22:46:07 +0800

    feat: 完成基础的据点生成以及保护

--- MBBattle/src/main/java/cc/venja/minebbs/battle/BattleMain.java
+++ MBBattle/src/main/java/cc/venja/minebbs/battle/BattleMain.java
@@ -1,28 +1,27 @@
 package cc.venja.minebbs.battle;
 
 import cc.venja.minebbs.login.enums.Team;
-import org.bukkit.Bukkit;
-import org.bukkit.GameMode;
-import org.bukkit.Location;
-import org.bukkit.World;
+import cc.venja.minebbs.robot.RobotMain;
+import com.destroystokyo.paper.event.player.PlayerPostRespawnEvent;
+import org.bukkit.*;
+import org.bukkit.block.Block;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.*;
+import org.bukkit.event.entity.EntityChangeBlockEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import com.destroystokyo.paper.event.player.PlayerPostRespawnEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.event.server.ServerLoadEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.checkerframework.checker.units.qual.N;
 
 import java.io.File;
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-
-import cc.venja.minebbs.robot.RobotMain;
+import java.util.*;
 
 public class BattleMain extends JavaPlugin implements Listener {
     public static BattleMain instance;
@@ -54,6 +53,17 @@ public class BattleMain extends JavaPlugin implements Listener {
                 configuration.set("TeamYELLOW", teamConfig);
 
                 configuration.set("RespawnCooldown", 20);
+
+                configuration.set("StrongHold", new ArrayList<>() {
+                    {
+                        add(new HashMap<String, Object>() {
+                            {
+                                put("Position", new int[] {0, 0, 0});
+                                put("OwnerTeam", "TeamRED");
+                            }
+                        });
+                    }
+                });
             }
 
             configuration.save(configFile);
@@ -80,7 +90,7 @@ public class BattleMain extends JavaPlugin implements Listener {
         Player player = event.getPlayer();
         Location location = event.getRespawnedLocation();
 
-        var teamValue = RobotMain.getPlayerTeam(event.getPlayer().getName());
+        Team teamValue = RobotMain.getPlayerTeam(event.getPlayer().getName());
         String team = "Team" + teamValue;
 
         List<Double> respawnPosition = Objects.requireNonNull(configuration.getConfigurationSection(team)).
@@ -99,5 +109,184 @@ public class BattleMain extends JavaPlugin implements Listener {
             player.teleport(respawnLocation);
             player.setGameMode(GameMode.SURVIVAL);
         }, configuration.getInt("RespawnCooldown") * 20L);
+    }
+
+    @EventHandler
+    public void onServerLoad(ServerLoadEvent event) {
+        List<Map<?, ?>> strongholdList = configuration.getMapList("StrongHold");
+        if (strongholdList.size() == 1) {
+            this.getLogger().warning("据点尚未配置，无法生成据点");
+            return;
+        }
+
+        World world = Bukkit.getWorld("world");
+
+        strongholdList.forEach(map -> {
+            if (world == null) return;
+
+            @SuppressWarnings("unchecked")
+            List<Integer> position = (List<Integer>) map.get("Position");
+            int x = position.get(0);
+            int y = position.get(1);
+            int z = position.get(2);
+
+            Block beacon = world.getBlockAt(x, y-1, z);
+            if (!beacon.getType().equals(Material.BEACON)) {
+                beacon.setType(Material.BEACON);
+
+                String ownerTeam = map.get("OwnerTeam").toString();
+
+                Material glassMaterial = switch (ownerTeam) {
+                    case "TeamRED" -> Material.RED_STAINED_GLASS;
+                    case "TeamBLUE" -> Material.BLUE_STAINED_GLASS;
+                    case "TeamGRAY" -> Material.GRAY_STAINED_GLASS;
+                    case "TeamYELLOW" -> Material.YELLOW_STAINED_GLASS;
+                    default -> Material.GLASS;
+                };
+
+                Block glass = world.getBlockAt(x, y, z);
+                glass.setType(glassMaterial);
+
+                Block ironBlock = null;
+                for (int x2 = -1; x2 < 2; x2++) {
+                    for (int z2 = -1; z2 < 2; z2++) {
+                        ironBlock = world.getBlockAt(x+x2, y-2, z+z2);
+                        ironBlock.setType(Material.IRON_BLOCK);
+                    }
+                }
+
+                this.getLogger().info(String.format("位于(%d, %d, %d)的%s据点已成功生成", x, y, z, ownerTeam));
+            }
+        });
+    }
+
+    @EventHandler
+    public void onBlockDamage(BlockDamageEvent event) {
+        event.setCancelled(protectAreaOfStrongHold(event.getBlock()));
+    }
+
+    @EventHandler
+    public void onBlockExplode(BlockExplodeEvent event) {
+        List<Block> blocks = event.blockList();
+        for (Block block: blocks) {
+            if (protectAreaOfStrongHold(block)) {
+                event.setCancelled(true);
+                break;
+            }
+        }
+    }
+
+    @EventHandler
+    public void onBlockPistonExtend(BlockPistonExtendEvent event) {
+        List<Block> blocks = event.getBlocks();
+        for (Block block: blocks) {
+            if (protectAreaOfStrongHold(block)) {
+                event.setCancelled(true);
+                break;
+            }
+        }
+    }
+
+    @EventHandler
+    public void onBlockPistonRetract(BlockPistonRetractEvent event) {
+        List<Block> blocks = event.getBlocks();
+        for (Block block: blocks) {
+            if (protectAreaOfStrongHold(block)) {
+                event.setCancelled(true);
+                break;
+            }
+        }
+    }
+
+    @EventHandler
+    public void onEntityChangeBlock(EntityChangeBlockEvent event) {
+        event.setCancelled(protectAreaOfStrongHold(event.getBlock()));
+    }
+
+    @EventHandler
+    public void onEntityExplode(EntityExplodeEvent event) {
+        List<Block> blocks = event.blockList();
+        for (Block block: blocks) {
+            if (protectAreaOfStrongHold(block)) {
+                event.setCancelled(true);
+                break;
+            }
+        }
+    }
+
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent event) {
+        event.setCancelled(protectAreaOfStrongHold(event.getBlock()));
+    }
+
+    private boolean protectAreaOfStrongHold(Block block) {
+        List<Map<?, ?>> strongholdList = configuration.getMapList("StrongHold");
+
+        World world = block.getWorld();
+        Location location = block.getLocation();
+
+        for (Map<?, ?> map : strongholdList) {
+            @SuppressWarnings("unchecked")
+            List<Integer> position = (List<Integer>) map.get("Position");
+            int x = position.get(0);
+            int y = position.get(1);
+            int z = position.get(2);
+
+            Location minLocation = new Location(world, x, y, z).subtract(5, 5, 5);
+            Location maxLocation = new Location(world, x, y, z).add(5, 5, 5);
+
+            if (location.getBlockX() >= minLocation.getBlockX() && location.getBlockX() <= maxLocation.getBlockX()) {
+                if (location.getBlockY() >= minLocation.getBlockY() && location.getBlockY() <= maxLocation.getBlockY()) {
+                    if (location.getBlockZ() >= minLocation.getBlockZ() && location.getBlockZ() <= maxLocation.getBlockZ()) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    @Deprecated
+    private boolean protectStrongHold(Block block) {
+        List<Map<?, ?>> strongholdList = configuration.getMapList("StrongHold");
+
+        World world = block.getWorld();
+
+        Location location = block.getLocation();
+        for (Map<?, ?> map : strongholdList) {
+            @SuppressWarnings("unchecked")
+            List<Integer> position = (List<Integer>) map.get("Position");
+            int x = position.get(0);
+            int y = position.get(1);
+            int z = position.get(2);
+
+            Location nowLocation = new Location(world, x, y, z);
+
+            if (location.equals(nowLocation)) {
+                return block.getType().equals(Material.RED_STAINED_GLASS) ||
+                        block.getType().equals(Material.BLUE_STAINED_GLASS) ||
+                        block.getType().equals(Material.GRAY_STAINED_GLASS) ||
+                        block.getType().equals(Material.YELLOW_STAINED_GLASS);
+            }
+
+            nowLocation.subtract(0, 1, 0);
+
+            if (location.equals(nowLocation)) {
+                return block.getType().equals(Material.BEACON);
+            }
+
+            for (int x2 = -1; x2 < 2; x2++) {
+                for (int z2 = -1; z2 < 2; z2++) {
+                    nowLocation.set(x+x2, y-2, z+z2);
+
+                    if (location.equals(nowLocation)) {
+                        if (block.getType().equals(Material.IRON_BLOCK)) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
