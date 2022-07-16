feb6acd403efc6daf61d68cd19ee5093ce8ac557
ViceEye <17517159+ViceEye@users.noreply.github.com>
2022-07-16 15:23:01 +0100

    feat!: 允许玩家重复申请白名单，重复验证归于机器人

--- MBRobot/src/main/java/cc/venja/minebbs/robot/handler/RobotWhitelistHandler.java
+++ MBRobot/src/main/java/cc/venja/minebbs/robot/handler/RobotWhitelistHandler.java
@@ -26,21 +26,16 @@ public class RobotWhitelistHandler implements HttpHandler {
             var respondDao = new RespondDao();
             Bukkit.getLogger().info(String.valueOf(playerDao.isValid()));
             if (playerDao.isValid()) {
-                if (RobotMain.existsWhitelist(playerDao.playerName)) {
-                    respondDao.respondCode = RespondDao.RespondCode.FAILED.getValue();
-                    respondDao.respondData = "Player already added to whitelist";
-                } else {
-                    RobotMain.addWhitelist(playerDao.playerName, playerDao.KHL);
-                    respondDao.respondCode = RespondDao.RespondCode.SUCCESS.getValue();
-                    try {
-                        Bukkit.getLogger().info(String.valueOf(RobotMain.getLowestMemberTeam()));
-                        RobotMain.addPlayerTeam(playerDao.playerName, RobotMain.getLowestMemberTeam().getValue(), playerDao.KHL);
-                        respondDao.respondData = String.valueOf(Objects.requireNonNull(RobotMain.getPlayerTeam(playerDao.playerName)).getValue());
-                    } catch (SQLException e) {
-                        RobotMain.instance.getLogger().info(e.toString());
-                    }
-                    Bukkit.getLogger().info(respondDao.respondData);
+                RobotMain.addWhitelist(playerDao.playerName, playerDao.KHL);
+                respondDao.respondCode = RespondDao.RespondCode.SUCCESS.getValue();
+                try {
+                    Bukkit.getLogger().info(String.valueOf(RobotMain.getLowestMemberTeam()));
+                    RobotMain.addPlayerTeam(playerDao.playerName, RobotMain.getLowestMemberTeam().getValue(), playerDao.KHL);
+                    respondDao.respondData = String.valueOf(Objects.requireNonNull(RobotMain.getPlayerTeam(playerDao.playerName)).getValue());
+                } catch (SQLException e) {
+                    RobotMain.instance.getLogger().info(e.toString());
                 }
+                Bukkit.getLogger().info(respondDao.respondData);
             } else {
                 respondDao.respondCode = RespondDao.RespondCode.FAILED.getValue();
                 respondDao.respondData = "POST Body invalid";
