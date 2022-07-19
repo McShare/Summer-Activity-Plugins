339b01e1ae1e3efc076a4defe81fc4e522c17684
McPlus <97387028+McPlusss@users.noreply.github.com>
2022-07-19 08:31:50 +0800

    feat: 新增队伍名字

--- MBLogin/src/main/java/cc/venja/minebbs/login/enums/Team.java
+++ MBLogin/src/main/java/cc/venja/minebbs/login/enums/Team.java
@@ -6,26 +6,35 @@ import java.util.HashMap;
 import java.util.Map;
 
 public enum Team {
-    RED(0),
-    BLUE(1),
-    GREY(2),
-    YELLOW(3),
-    IMPOSTER(4),
-    ADMIN(5);
+    RED(0, "TeamRED"),
+    BLUE(1, "TeamBLUE"),
+    GREY(2, "TeamGREY"),
+    YELLOW(3, "TeamYELLOW"),
+    IMPOSTER(4, "TeamIMPOSTER"),
+    ADMIN(5, "TeamADMIN");
 
     final int value;
+    final String name;
     static final Map<Integer, Team> BY_ID = new HashMap<>();
+    static final Map<String, Team> BY_NAME = new HashMap<>();
 
-    Team(int value) {
+    Team(int value, String name) {
         this.value = value;
+        this.name = name;
     }
 
     public int getValue() {
         return this.value;
     }
+    public String getName() {
+        return this.name;
+    }
 
     public static Team getByValue(int value) {
         return BY_ID.get(value);
+    }
+    public static Team getByName(String value) {
+        return BY_NAME.get(value);
     }
 
     public static String getColorCode(@NotNull Team team) {
@@ -49,6 +58,7 @@ public enum Team {
     static {
         for (Team team : values()) {
             BY_ID.put(team.getValue(), team);
+            BY_NAME.put(team.getName(), team);
         }
     }
 }
