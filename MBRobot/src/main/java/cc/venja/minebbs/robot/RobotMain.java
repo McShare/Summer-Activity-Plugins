4919cdd65b9fd1519b48422efa3902b30007ce56
ViceEye <17517159+ViceEye@users.noreply.github.com>
2022-07-26 00:59:45 +0100

    feat: 更新分配机制

--- MBRobot/src/main/java/cc/venja/minebbs/robot/RobotMain.java
+++ MBRobot/src/main/java/cc/venja/minebbs/robot/RobotMain.java
@@ -1,5 +1,6 @@
 package cc.venja.minebbs.robot;
 
+import cc.venja.minebbs.login.LoginMain;
 import cc.venja.minebbs.login.enums.Team;
 import cc.venja.minebbs.login.database.PlayerInfo;
 import cc.venja.minebbs.login.database.dao.PlayerInfoDao;
@@ -10,6 +11,7 @@ import cc.venja.minebbs.robot.handler.RobotWhitelistHandler;
 import com.google.gson.Gson;
 import com.sun.net.httpserver.HttpServer;
 import net.kyori.adventure.text.Component;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -26,7 +28,11 @@ import javax.annotation.Nullable;
 import java.io.*;
 import java.net.InetSocketAddress;
 import java.nio.charset.StandardCharsets;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executors;
@@ -92,6 +98,7 @@ public class RobotMain extends JavaPlugin implements Listener {
         getServer().getPluginManager().registerEvents(this, this);
         try {
             reloadTeamListMap();
+            reloadLatestTeamStat();
         } catch (Exception e) {
             getLogger().info(e.toString());
         }
@@ -120,8 +127,30 @@ public class RobotMain extends JavaPlugin implements Listener {
         if (command.getName().equals("doupdate")) {
             try {
                 reloadTeamListMap();
-            } catch (SQLException e) {
+                reloadLatestTeamStat();
+            } catch (Exception e) {
                 getLogger().info(e.toString());
+            }
+        }
+        if (command.getName().equals("analyse")) {
+            for (OfflinePlayer player : this.getServer().getOfflinePlayers()) {
+                try {
+                    String name = getRealPlayerName(player.getName());
+                    if (existsPlayerTeam(name)) {
+                        String sql = "insert into record(player_name, last_login_time, team) values(?,?,?)";
+
+                        PreparedStatement statement = LoginMain.instance.databaseConnection.prepareStatement(sql);
+
+                        statement.setString(1, Objects.requireNonNull(name).toLowerCase());
+                        statement.setDate(2, new Date(player.getLastLogin()));
+                        statement.setInt(3, Objects.requireNonNull(getPlayerTeam(name)).getValue());
+
+                        getLogger().info(statement.toString().replace("com.mysql.cj.jdbc.ClientPreparedStatement: ", ""));
+                        statement.execute();
+                    }
+                } catch (SQLException exception) {
+                    getLogger().info(exception.toString());
+                }
             }
         }
         return false;
@@ -132,9 +161,71 @@ public class RobotMain extends JavaPlugin implements Listener {
         for (Team team : Team.values()) {
             List<String> players = new ArrayList<>();
             for (PlayerInfo playerInfo : dao.queryPlayersByTeam(team.getValue())) {
+                instance.getLogger().info(team + " > 缓存 > " + playerInfo.getPlayerName());
                 players.add(playerInfo.getPlayerName());
             }
             teamListMap.put(team, players);
+        }
+    }
+
+    static SimpleDateFormat f = new SimpleDateFormat("yyyy-MM-dd");
+    public static Map<Team, List<String>> newTeamListMap = new ConcurrentHashMap<>();
+
+    public static void reloadLatestTeamStat() throws Exception {
+        String sql = "select * from record";
+        PreparedStatement statement = LoginMain.instance.databaseConnection.prepareStatement(sql);
+
+        ResultSet resultSet = statement.executeQuery();
+        List<Info> filteredInfo = new ArrayList<>();
+
+        while (resultSet.next()) {
+            Info info = new Info(
+                    resultSet.getString("player_name"),
+                    resultSet.getDate("last_login_time"),
+                    resultSet.getInt("team")
+            );
+            java.util.Date d = f.parse("2022-07-25");
+            if (info.date >= d.getTime()) {
+                filteredInfo.add(info);
+                instance.getLogger().info(info.toString());
+            }
+        }
+        instance.getLogger().info(String.valueOf(filteredInfo.size()));
+
+        for (Info info : filteredInfo) {
+            List<String> players = newTeamListMap.getOrDefault(info.team, new ArrayList<>());
+            players.add(info.playerName.toLowerCase());
+            newTeamListMap.put(info.team, players);
+        }
+
+        int num = 0;
+        for (List<String> value : newTeamListMap.values()) {
+            num += value.size();
+        }
+        instance.getLogger().info(String.valueOf(num));
+    }
+
+    public static class Info {
+        public String playerName;
+        public Long date;
+        public Team team;
+
+        public Info() {
+        }
+
+        public Info(String playerName, Date date, int team) {
+            this.playerName = playerName;
+            this.date = date.getTime();
+            this.team = Team.getByValue(team);
+        }
+
+        @Override
+        public String toString() {
+            return "Info{" +
+                    "playerName='" + playerName + '\'' +
+                    ", date=" + date +
+                    ", team=" + team +
+                    '}';
         }
     }
 
@@ -172,6 +263,11 @@ public class RobotMain extends JavaPlugin implements Listener {
         List<String> players = teamListMap.get(Team.getByValue(team));
         players.add(player);
         teamListMap.put(Team.getByValue(team), players);
+
+        players.clear();
+        players = newTeamListMap.get(Team.getByValue(team));
+        players.add(player);
+        newTeamListMap.put(Team.getByValue(team), players);
     }
 
     public static void removePlayerTeam(String player) throws SQLException {
@@ -182,6 +278,11 @@ public class RobotMain extends JavaPlugin implements Listener {
         List<String> players = teamListMap.get(team);
         players.remove(player);
         teamListMap.put(team, players);
+
+        players.clear();
+        players = newTeamListMap.get(team);
+        players.remove(player);
+        newTeamListMap.put(team, players);
     }
 
     private static void updatePlayer(String player, int team) throws SQLException {
@@ -244,7 +345,7 @@ public class RobotMain extends JavaPlugin implements Listener {
                 return floodgatePlayer.getUsername();
             }
         }
-        return player;
+        return player.replace("BE+", "");
     }
 
     public static Team getLowestMemberTeam() {
